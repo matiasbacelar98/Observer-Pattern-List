0c064526c8220983043c96d1f50cc780223c766a
Matias Bacelar <matiasbacelar@gmail.com>
2022-12-24 15:21:24 -0300

    Project init

---
+++ src/app.js
@@ -0,0 +1,3 @@
+document.querySelector('#app').innerHTML = ` 
+ <h1>Project List with the Observer Pattern</h1>
+`;
---
+++ src/constants/users.js
@@ -0,0 +1,15 @@
+// Initial users
+export default [
+  {
+    id: 1,
+    name: 'Jennifer',
+  },
+  {
+    id: 2,
+    name: 'Jane',
+  },
+  {
+    id: 3,
+    name: 'John',
+  },
+];
